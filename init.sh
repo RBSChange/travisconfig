775c5e3137d0bc1e3a1e4dca53af762fdc03856e
Franck STAUFFER <franck.stauffer@rbs.fr>
2012-10-03 15:53:47 +0200

    Config updated

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cp travisconfig/profile profile
 cp travisconfig/change.properties change.properties 
-cp -R travisconfig/config config
+cp travisconfig/config/project.xml App/Config/project.xml
+cp travisconfig/config/project.default.xml App/Config/project.default.xml
 cp -R travisconfig/install.xml install.xml
 sed -i 's|<current_dir>|'`pwd`'|g' change.properties
