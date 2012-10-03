495c025d6f3de92e39cfa5ea42ddb97f600a58a0
Franck STAUFFER <franck.stauffer@rbs.fr>
2012-10-03 16:35:18 +0200

    Create directory

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 cp travisconfig/profile profile
 cp travisconfig/change.properties change.properties 
+mkdir -p App/Config
 cp travisconfig/config/project.xml App/Config/project.xml
 cp travisconfig/config/project.default.xml App/Config/project.default.xml
 cp -R travisconfig/install.xml install.xml
