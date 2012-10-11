141e6414ddfa3b92e6be7548cee63a0b1e26659d
Franck STAUFFER <franck.stauffer@rbs.fr>
2012-10-11 16:00:57 +0200

    New change.json file

--- init.sh
+++ init.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 cp travisconfig/profile profile
-cp travisconfig/change.properties change.properties 
+cp travisconfig/change.properties change.properties
+cp travisconfig/change.json change.json 
 mkdir -p App/Config
 cp travisconfig/config/project.xml App/Config/project.xml
 cp travisconfig/config/project.default.xml App/Config/project.default.xml
 cp -R travisconfig/install.xml install.xml
 sed -i 's|<current_dir>|'`pwd`'|g' change.properties
+sed -i 's|<current_dir>|'`pwd`'|g' change.json
