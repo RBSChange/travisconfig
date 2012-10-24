21c7ed18dc48714be47582131ef50c6d48ed1ef3
Franck STAUFFER <franck.stauffer@rbs.fr>
2012-10-24 14:32:08 +0200

    Update JSON

--- init.sh
+++ init.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 cp travisconfig/profile profile
-cp travisconfig/change.properties change.properties
-cp travisconfig/change.json change.json 
 mkdir -p App/Config
-cp travisconfig/config/project.xml App/Config/project.xml
-cp travisconfig/config/project.default.xml App/Config/project.default.xml
+mkdir -p ChangeTests/UnitTestWorkspace/App/Config
+cp travisconfig/config/project.json App/Config/project.json
+cp travisconfig/config/project.default.json App/Config/project.default.json
+cp travisconfig/config/project.json ChangeTests/UnitTestWorkspace/App/Config/project.json
+cp travisconfig/config/project.default.json ChangeTests/UnitTestWorkspace/App/Config/project.default.json
 cp -R travisconfig/install.xml install.xml
-sed -i 's|<current_dir>|'`pwd`'|g' change.properties
-sed -i 's|<current_dir>|'`pwd`'|g' change.json
+sed -i 's|<current_dir>|'`pwd`'|g' App/Config/project.default.json
+sed -i 's|<current_dir>|'`pwd`'|g' ChangeTests/UnitTestWorkspace/App/Config/project.default.json
